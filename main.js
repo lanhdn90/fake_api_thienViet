d61b6c287c1499aedc7ebc1c71d0acc7ae139ab4
lanhdn90 <lanhdn90@gmail.com>
2022-06-07 15:19:23 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const jsonServer = require("json-server");
+const queryString = require("query-string");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// Add custom routes before JSON Server router
+server.get("/echo", (req, res) => {
+  res.jsonp(req.query);
+});
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+router.render = (rep, res) => {
+  const headers = res.getHeaders();
+  const totalCountHeader = headers["x-total-count"];
+  if (rep.method === "GET" && totalCountHeader) {
+    const queryParams = queryString.parse(rep._parsedUrl.query);
+    const result = {
+      data: res.locals.data,
+      _page: Number.parseInt(queryParams._page),
+      _limit: Number.parseInt(queryParams._limit),
+      totalElement: Number.parseInt(totalCountHeader),
+      hasNext:
+        Number.parseInt(totalCountHeader) > Number.parseInt(queryParams._limit)
+          ? true
+          : false,
+    };
+    return res.jsonp(result);
+  }
+  res.jsonp(res.locals.data);
+};
+
+// Use default router
+server.use("/api", router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
